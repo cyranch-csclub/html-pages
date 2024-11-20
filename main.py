52d23376b2e008331bb9ce15c0d872d2e2d9d161
acemavrick <116685696+acemavrick@users.noreply.github.com>
2024-11-19 23:09:27 -0600

    Added demo files

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ cursor = connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS pages (name TEXT, code TEXT)")
 connection.commit()
 
+# build the raw HTML code of the demo page and save it as a text file (demo.html)
+# the goal is to make the raw HTML code of the demo page accessible to the user
+with (open("templates/demo.txt", "w")) as f:
+    f.write(open("templates/demo.html").read().replace("<", "&lt;")
+            .replace(">", "&gt;").replace("\n", "<br>")
+            .replace(" ", "&nbsp;"))
+
+# same with css
+with (open("static/demo_style.txt", "w")) as f:
+    f.write(open("static/demo_style.css").read().replace("<", "&lt;")
+            .replace(">", "&gt;").replace("\n", "<br>")
+            .replace(" ", "&nbsp;"))
+
 @app.route("/")
 def main():
     con = sqlite3.connect("main.db")
@@ -14,10 +27,25 @@ def main():
     page_list = cur.execute("SELECT name, rowid FROM pages").fetchall()
     return render_template("index.html", pages=page_list)
 
+@app.route("/demo")
+@app.route("/demo/")
+def demo():
+    return render_template("demo.html")
+
+@app.route("/demo/html")
+@app.route("/demo/html/")
+def html():
+    return open("templates/demo.txt").read()
+
+@app.route("/demo/css")
+@app.route("/demo/css/")
+def css():
+    return open("static/demo_style.txt").read()
+
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
     message = ""
-    if (request.method == "POST"):
+    if request.method == "POST":
         name = request.form["name"]
         code = request.form["code"]
         print(code)
